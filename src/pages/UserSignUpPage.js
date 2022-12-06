6c3606773fb58bbcaf6ce99a9edd5a333dbd9cea
lara <lahirurajapakshe.stack@gmail.com>
2022-12-06 22:15:07 +0530

    Finalize signup page

--- src/pages/UserSignUpPage.js
+++ src/pages/UserSignUpPage.js
@@ -1,11 +1,81 @@
 import React, { Component } from 'react'
 
 export class UserSignUpPage extends Component {
+
+    state={
+        displayName:'',
+        username:'',
+        password:'',
+        passwordRepeat:''
+
+    };
+    
+    onChangeDisplayName=(event)=>{
+        const value=event.target.value;
+
+        this.setState({displayName:value})
+
+    };
+
+    onChangeUserName=(event)=>{
+        const value=event.target.value;
+
+        this.setState({username:value})
+
+    };
+
+    onChangePassword=(event)=>{
+        const value=event.target.value;
+
+        this.setState({password:value})
+
+    };
+
+    onChangePasswordRepeat=(event)=>{
+        const value=event.target.value;
+
+        this.setState({passwordRepeat:value})
+
+    };
+    onClickSignUp =()=>{
+        const user={
+            username: this.state.username,
+            displayName:this.state.displayName,
+            password:this.state.password
+        }
+        this.props.actions.postSignup(user);
+    };
   render() {
     return (
-      <div><h1>Sign Up</h1></div>
+      <div>
+        <h1>Sign Up</h1>
+      <input placeholder='your display name' value={this.state.displayName} onChange={this.onChangeDisplayName}/>
+      <br></br>
+      <br></br>
+      <input placeholder='your username'  value={this.state.username} onChange={this.onChangeUserName}/>
+      <br></br>
+      <br></br>
+      <input placeholder='your password' type='password' value={this.state.password} onChange={this.onChangePassword}/>
+      <br></br>
+      <br></br>
+      <input placeholder='repeat your password' type='password' value={this.state.passwordRepeat} onChange={this.onChangePasswordRepeat}/>
+      <br></br>
+      <br></br>
+      
+      <button onClick={this.onClickSignUp}>Signup</button>
+      </div>
+
     )
   }
 }
 
-export default UserSignUpPage
+UserSignUpPage.defaultProps={
+    actions:{
+        postSignup:()=> new Promise((resolve,reject)=>{
+            resolve({});
+        })
+    
+}
+};
+
+export default UserSignUpPage;
